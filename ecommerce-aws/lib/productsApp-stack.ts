7ed7444c4a43a311900a30cbc4008b972ae57433
Vyctor <dev.vyctor@gmail.com>
2023-02-04 21:22:24 -0300

    criando a tabela no dynamodb com o aws cdk

--- ecommerce-aws/lib/productsApp-stack.ts
+++ ecommerce-aws/lib/productsApp-stack.ts
@@ -1,13 +1,26 @@
-import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as lambdaNodeJs from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class ProductsAppStack extends cdk.Stack {
   public readonly productsFetchHandler: lambdaNodeJs.NodejsFunction;
+  public readonly productsDynamoDb: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    this.productsDynamoDb = new dynamodb.Table(this, 'ProductsDynamoDb', {
+      tableName: 'products',
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: {
+        name: 'id',
+        type: dynamodb.AttributeType.STRING,
+      },
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+    });
 
     this.productsFetchHandler = new lambdaNodeJs.NodejsFunction(this, 'ProductsFetchFunction', {
       functionName: 'ProductsFetchFunction',
@@ -18,6 +31,9 @@ export class ProductsAppStack extends cdk.Stack {
       bundling: {
         minify: true,
         sourceMap: false,
+      },
+      environment: {
+        PRODUCTS_TABLE_NAME: this.productsDynamoDb.tableName,
       },
     });
   }
